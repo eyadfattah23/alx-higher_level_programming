c95fa91a7ae5013a9ef82ac015d2e4064c20189c
eyadfattah23 <eyadabdelfattah4@gmail.com>
2023-06-05 22:33:10 +0300

    remove try and except

--- 0x00-python-hello_world/100-write.py
+++ 0x00-python-hello_world/100-write.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
 import sys
 
-try:
-    sys.stdout.write("and that piece of art is useful - Dora Korpar, 2015-10-19")
-except:
-    sys.stderr.write("write")
-    exit(1)
+sys.stdout.write("and that piece of art is useful\
+    - Dora Korpar, 2015-10-19")
+sys.exit(1)
