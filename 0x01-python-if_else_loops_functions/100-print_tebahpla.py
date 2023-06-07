794673b03c7bcbc87662c2b0f5463df2eeada759
eyadfattah23 <eyadabdelfattah4@gmail.com>
2023-06-07 05:47:27 +0300

    use print with string format

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 for c in range(ord('z'), ord('a')-1, -1):
     if c % 2 == 0:
-        print(chr(c).lower(), end='')
+        print("{}".format(chr(c).lower()), end='')
     else:
-        print(chr(c).upper(), end='')
+        print("{}".format(chr(c).upper()), end='')
