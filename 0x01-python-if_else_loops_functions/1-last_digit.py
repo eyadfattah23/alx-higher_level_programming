26e00c97e1afda422e13fb40fe33925275af3bc6
eyadfattah23 <eyadabdelfattah4@gmail.com>
2023-06-07 03:09:33 +0300

    update to handle the - =numbers correctly

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -4,11 +4,16 @@ number = random.randint(-10000, 10000)
 num = number
 if number < 0:
     num = -number
-print("Last digit of {} is {} ".format(number, num % 10), end="")
-if num % 10 > 5:
+    num = num % 10
+    num *= -1
+else:
+    num = number % 10
+print("Last digit of {} is {} ".format(number, num), end="")
+    
+if num > 5:
     print("and is greater than 5")
-elif num % 10 == 0:
+elif num == 0:
     print("and is 0")
-elif num % 10 < 6 and num % 10 != 0:
+elif num < 6 and num != 0:
     print("and is less than 6 and not 0")
     
