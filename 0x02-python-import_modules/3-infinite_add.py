f32472f8edf91ec237f8c037c83126298ad43711
eyadfattah23 <eyadabdelfattah4@gmail.com>
2023-06-08 11:02:09 +0300

    add if __name__ == "__main__": line code should not be executed when imported

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,11 +1,10 @@
 #!/usr/bin/python3
 import sys
-
-sum = 0
-if (len(sys.argv)) == 1:
-    print("0")
-else:
-    for i in range(1, len(sys.argv)):
-        sum += int(sys.argv[i])
-    print("{}".format(sum))
-    
+if __name__ == "__main__":
+    sum = 0
+    if (len(sys.argv)) == 1:
+        print("0")
+    else:
+        for i in range(1, len(sys.argv)):
+            sum += int(sys.argv[i])
+        print("{}".format(sum))
