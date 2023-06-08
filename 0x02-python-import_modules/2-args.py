aaa95b5a910b04715c05578bc420d787a3f13067
eyadfattah23 <eyadabdelfattah4@gmail.com>
2023-06-08 11:01:52 +0300

    change if __name__ == "__main__": line code should not be executed when imported

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 
 import sys
-
-if (len(sys.argv)) == 1:
-    print("0 arguments.")
-else:
-    print("{} arguments:".format(len(sys.argv) - 1))
-    for i in range(1, len(sys.argv)):
-        print("{}: {}".format(i, sys.argv[i]))
+if __name__ == "__main__":
+    if (len(sys.argv)) == 1:
+        print("0 arguments.")
+    else:
+        print("{} arguments:".format(len(sys.argv) - 1))
+        for i in range(1, len(sys.argv)):
+            print("{}: {}".format(i, sys.argv[i]))
