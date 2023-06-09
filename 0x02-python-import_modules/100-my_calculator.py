b85c7a32d9786ac5a1d418d928da2abf429397cb
eyadfattah23 <eyadabdelfattah4@gmail.com>
2023-06-09 04:13:09 +0300

    pycodestyle fix

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
 
 if __name__ == "__main__":
-	from calculator_1 import add, sub, mul, div
-	import sys
+    from calculator_1 import add, sub, mul, div
+    import sys
 
 if len(sys.argv) != 4:
     print("Usage: ./100-my_calculator.py <a> <operator> <b>")
     sys.exit(1)
-a= int(sys.argv[1])
-b= int(sys.argv[3])
+a = int(sys.argv[1])
+b = int(sys.argv[3])
 if sys.argv[2] == "+":
     print(f"{a} + {b} = {add(a, b)}")
 elif sys.argv[2] == "-":
