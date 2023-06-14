066889e01352b911ac3602f93a5c2d95e2d2c577
eyadfattah23 <eyadabdelfattah4@gmail.com>
2023-06-14 06:26:06 +0300

    update handling empty list and non list args

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
-    n = my_list[0]
-    for i in my_list:
-        if i > n:
-            n = i
-    return n
+    if isinstance(my_list, list):
+        if len(my_list) == 0:
+            return None
+        n = my_list[0]
+        for i in my_list:
+            if i > n:
+                n = i
+        return n
