a958d8480767f16a606a81f8db225973df5943d0
eyadfattah23 <eyadabdelfattah4@gmail.com>
2023-06-27 02:49:59 +0300

    fix handle out of range x case

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -7,8 +7,9 @@ def safe_print_list(my_list=[], x=0):
             i += 1
         print()
     except IndexError:
-        while i < 5:
-            print(f"{my_list[i]}", end='')
+        i = 0
+        for j in my_list:
+            print(f"{j}", end='')
             i += 1
         print()
     return i
