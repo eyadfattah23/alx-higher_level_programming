f226009a25c9631275160771429b6f7fa3aab160
eyadfattah23 <eyadabdelfattah4@gmail.com>
2023-06-27 04:44:10 +0300

    keep out index error

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -5,7 +5,7 @@ def safe_print_list_integers(my_list=[], x=0):
         try:
             print("{:d}".format(my_list[index]), end="")
             i += 1
-        except Exception:
+        except (ValueError, TypeError):
             continue
     print()
     return (i)
