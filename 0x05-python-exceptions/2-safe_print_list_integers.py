c33be7722e316152609ec9d381f0df07a95c3606
eyadfattah23 <eyadabdelfattah4@gmail.com>
2023-06-29 05:46:27 +0300

    update 1- using for loop  and adding another condition 2-using pass instead of continue 3-depend on another counter instead of i

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
 def safe_print_list_integers(my_list=[], x=0):
-    i = 0
-    for index in range(0, x):
+    i, n = 0, 0
+    while i < x and my_list[i]:
         try:
-            print("{:d}".format(my_list[index]), end="")
-            i += 1
+            print("{:d}".format(my_list[i]), end= '')
+            n += 1
         except (ValueError, TypeError):
-            continue
+            pass
+        i += 1
     print()
-    return (i)
+    return n
