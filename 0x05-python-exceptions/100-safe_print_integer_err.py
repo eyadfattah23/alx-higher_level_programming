0aa4196c22ae4562d2e5b40ffa3872bf9cf754dc
eyadfattah23 <eyadabdelfattah4@gmail.com>
2023-06-29 06:47:40 +0300

    add spaces

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -6,9 +6,9 @@ def safe_print_integer_err(value):
     try:
         print('{:d}'.format(value))
     except ValueError as ve:
-        print('Exception:{}'.format(ve), file=sys.stderr)
+        print('Exception: {}'.format(ve), file=sys.stderr)
         i = False
     except TypeError as te:
-        print('Exception:{}'.format(te), file=sys.stderr)
+        print('Exception: {}'.format(te), file=sys.stderr)
         i = False
     return i
