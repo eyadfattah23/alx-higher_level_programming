762049750ad274f6d982c340094faf29b1d6b741
eyadfattah23 <eyadabdelfattah4@gmail.com>
2023-06-29 05:19:54 +0300

    try to use value error

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 def safe_print_integer(value):
+    i = True
     try:
-        print("{:d}".format(value))
-        return True
-    except Exception:
-        return False
+        print('{:d}'.format(value))
+    except ValueError:
+        i = False
+    return i
