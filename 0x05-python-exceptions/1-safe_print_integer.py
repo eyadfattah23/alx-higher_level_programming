4c0c52307a6f1724dd09326993832a5392fade86
eyadfattah23 <eyadabdelfattah4@gmail.com>
2023-06-29 05:22:24 +0300

    handle type error

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -3,6 +3,6 @@ def safe_print_integer(value):
     i = True
     try:
         print('{:d}'.format(value))
-    except ValueError:
+    except (ValueError, TypeError):
         i = False
     return i
