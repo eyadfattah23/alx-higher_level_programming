12510f7641c3f650ed447597d6e834fbeb860c9b
eyadfattah23 <eyadabdelfattah4@gmail.com>
2023-06-29 05:54:10 +0300

    remove type error

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -2,10 +2,8 @@
 def safe_print_division(a, b):
     try:
         res = a / b
-
-    except (TypeError, ZeroDivisionError):
+    except ZeroDivisionError:
         res = None
-
     finally:
         print("Inside result: {}".format(res))
-        return res
+    return res
