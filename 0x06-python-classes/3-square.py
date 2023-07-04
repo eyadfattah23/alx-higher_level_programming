9eb9dce0b939884de3ee1ad49db96e8c25f67ab9
eyadfattah23 <eyadabdelfattah4@gmail.com>
2023-07-04 06:27:26 +0300

    create class Square add area function

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+'''defines a square by:
+
+    Private instance attribute: size
+    Instantiation with size (no type/value verification)
+    Public instance method: def area(self):  returns the current square area
+'''
+
+
+class Square:
+    """define a square
+
+    Raises:
+            TypeError: size not an integer,
+            ValueError: size is less than 0
+    """
+    def __init__(self, size=0):
+        """initializes a square
+
+        Args:
+            size (int): size of the square
+
+        Raises:
+            TypeError: size not an integer,
+            ValueError: size is less than 0
+        """
+        self.__size = size
+        if not isinstance(size, int):
+            raise TypeError('size must be an integer')
+        if self.__size < 0:
+            raise ValueError('size must be >= 0')
+
+    def area(self):
+        """calculates area of the square
+
+        Returns:
+            int: area of the square
+        """
+        return self.__size ** 2
