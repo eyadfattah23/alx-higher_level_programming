e19bbdc385941ce1e687cdd272062b19f55f41e7
eyadfattah23 <eyadabdelfattah4@gmail.com>
2023-07-05 04:20:14 +0300

    pyc style fix

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -82,11 +82,19 @@ class Square:
 
     @position.setter
     def position(self, value):
+        """set position of the square
+
+        Args:
+            value (tuple): coordinates of the square
+
+        Raises:
+            TypeError: if value isn't a tuple of 2 + ints
+        """
         if not isinstance(value, tuple) \
             or len(value) != 2 \
-            	or not isinstance(value[0], int) \
-            	    or not isinstance(value[1], int) \
-            	        or value[0] < 0 or value[1] < 0:
+                or not isinstance(value[0], int) \
+                or not isinstance(value[1], int) \
+                or value[0] < 0 or value[1] < 0:
             raise TypeError('position must be a tuple of 2 positive integers')
         else:
             self.__position = value
