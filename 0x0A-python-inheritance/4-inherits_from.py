0f2ba3600357ea29e0ab2770d2dd09e3e5aa7a19
eyadfattah23 <eyadabdelfattah4@gmail.com>
2023-07-11 05:57:27 +0300

    pyc style fix

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-"""Write a function that returns True if the object is an instance of a class that 
+"""Write a function that returns True if the object
+is an instance of a class that
 inherited (directly or indirectly)from the specified class;
 """
 
