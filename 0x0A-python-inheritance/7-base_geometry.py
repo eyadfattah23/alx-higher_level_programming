040f5dbceec60380d7859dfa0c5d77d8fddb4b44
eyadfattah23 <eyadabdelfattah4@gmail.com>
2023-07-11 10:12:37 +0300

    use type instead of is instance

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -22,7 +22,7 @@ class BaseGeometry():
             ValueError: if value is less or equal to 0
             TypeError: if value is not an integer
         """
-        if not isinstance(value, int):
+        if not type(value) == int:
             raise TypeError(f"{name} must be an integer")
         if value <= 0:
             raise ValueError(f"{name} must be greater than 0")
