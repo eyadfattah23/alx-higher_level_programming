390a4aa220ad85c42ea6125f52ca96b7069ff0a8
eyadfattah23 <eyadabdelfattah4@gmail.com>
2023-07-12 05:47:51 +0300

    pyc style fix

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -5,8 +5,9 @@ and
 returns the number of characters added
 """
 
+
 def append_write(filename="", text=""):
-    """appends a string at the end of a text file (UTF8) 
+    """appends a string at the end of a text file (UTF8)
         and returns the number of characters added
     """
     with open(filename, 'a', encoding='utf-8') as f:
