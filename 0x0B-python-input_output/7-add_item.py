63090d435a402dafd53eb8db27f2054c65a1effa
eyadfattah23 <eyadabdelfattah4@gmail.com>
2023-07-13 06:06:40 +0300

    fix

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -4,12 +4,16 @@ and then save them to a file"""
 
 
 import sys
+import os
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 
 file = "add_item.json"
-plist = load_from_json_file(file)
-with open(file, 'w') as f:
+if os.path.exists(file):
+    plist = load_from_json_file(file)
+else:
+    plist = []
+with open(file, 'a') as f:
     for i in range(1, len(sys.argv)):
         plist.append(sys.argv[i])
 save_to_json_file(plist, file)
