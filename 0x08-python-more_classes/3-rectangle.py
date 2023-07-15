3851790d3f30424932cf9915c63d2aee452b38d1
eyadfattah23 <eyadabdelfattah4@gmail.com>
2023-07-15 12:18:42 +0300

    fix

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -93,7 +93,8 @@ class Rectangle:
         for i in range(self.__height):
             for j in range(self.__width):
                 rec += '#'
-            rec += '\n'
+            if i != self.__height - 1:
+                rec += '\n'
         return rec
         
         
