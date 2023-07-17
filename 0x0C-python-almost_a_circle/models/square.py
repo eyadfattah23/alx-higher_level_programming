0a914f0b59946cbe1b6c4a96a8bce9649bd99a0b
eyadfattah23 <eyadabdelfattah4@gmail.com>
2023-07-17 20:26:37 +0300

    create square class

---
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -0,0 +1,53 @@
+#!/usr/bin/python3
+"""define class square that inherits from rectangle"""
+
+from models.rectangle import Rectangle
+
+
+class Square(Rectangle):
+    """class square that inherits from rectangle"""
+
+    def __init__(self, size, x=0, y=0, id=None):
+        """initialize square"""
+        super().__init__(size, size, x, y, id)
+
+    def __str__(self):
+        """str representation of the rectangle
+        """
+        return f"[Square] ({self.id}) {self.x}/{self.y}" \
+            f" - {self.width}/{self.height}"
+
+    @property
+    def size(self):
+        """get size of the square"""
+        return self.width
+
+    @size.setter
+    def size(self, size):
+        """set size of the square"""
+        self.width = size
+        self.height = size
+
+    def update(self, *args, **kwargs):
+        """Update the class Rectangle"""
+
+        if args and len(args) > 0:
+            for i, arg in enumerate(args):
+                if i == 0:
+                    self.id = arg
+                elif i == 1:
+                    self.__size = arg
+                elif i == 2:
+                    self.__x = arg
+                elif i == 3:
+                    self.__y = arg
+        elif kwargs:
+            for k in kwargs:
+                if k == 'id':
+                    self.id = kwargs[k]
+                elif k == 'size':
+                    self.__size = kwargs[k]
+                elif k == 'x':
+                    self.__x = kwargs[k]
+                elif k == 'y':
+                    self.__y = kwargs[k]
