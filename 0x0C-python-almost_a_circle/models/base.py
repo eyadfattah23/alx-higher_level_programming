4627679b0a5f13036a7c3fe6c3d4bb70ddadcff0
eyadfattah23 <eyadabdelfattah4@gmail.com>
2023-07-17 15:29:37 +0300

    create readme empty file __init__.py models/base.py

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""define class base"""
+
+
+class Base:
+    """the base of all other classes in this project"""
+
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        """initialization function
+
+        Args:
+            id (_type_, optional): _description_. Defaults to None.
+        """
+        if id is not None:
+            self.id = id
+        else:
+            __nb_objects += 1
+            self.id = __nb_objects
