33abaa48630442a930e563d68b18cc96ded40618
eyadfattah23 <eyadabdelfattah4@gmail.com>
2023-07-18 05:24:07 +0300

    try again

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -21,8 +21,4 @@ class Base:
 
     def to_json_string(list_dictionaries):
         """returns the JSON string representation of list_dictionaries"""
-        if not list_dictionaries or not list_dictionaries[0] or list_dictionaries[0] == {}:
-            return "[]"
-        else:
-            import json
-            return json.dumps(list_dictionaries)
+        return json.dumps(list_dictionaries)
