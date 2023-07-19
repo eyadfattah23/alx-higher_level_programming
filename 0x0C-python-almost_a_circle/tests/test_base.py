edc0058a3920e571ab34511349e0091ac7a44857
eyadfattah23 <eyadabdelfattah4@gmail.com>
2023-07-19 16:24:24 +0300

    add test to_json_string

--- 0x0C-python-almost_a_circle/tests/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_base.py
@@ -2,12 +2,17 @@
 
 
 import unittest
-from models import base
-Base = base.Base
+from models.base import Base
+from models.rectangle import Rectangle
+from models.square import Square
+from json import load, loads, dump, dumps
 
 
 class TestBase(unittest.TestCase):
     """base class test class"""
+
+    def tearDown(self) -> None:
+        return super().tearDown()
 
     def test_init(self):
         """test initialization"""
@@ -21,3 +26,35 @@ class TestBase(unittest.TestCase):
         self.assertAlmostEqual(b4.id, 12)
         b5 = Base()
         self.assertAlmostEqual(b5.id, 4)
+
+    def test_json_string(self):
+        """test to_json_string"""
+        r1 = Rectangle(10, 7, 2, 8)
+        s1 = Square(5, 2, 3, 1)
+
+        dictionary = r1.to_dictionary()
+        json_dictionary = Base.to_json_string([dictionary])
+        self.assertDictEqual(
+            dictionary, {'x': 2, 'width': 10, 'id': 5, 'height': 7, 'y': 8})
+        self.assertIsInstance(json_dictionary, str)
+        self.assertIsInstance(dictionary, dict)
+        self.assertListEqual(
+            loads(json_dictionary),
+            loads('[{"x": 2, "width": 10, "id": 5, "height": 7, "y": 8}]')
+        )
+
+        dictionary = s1.to_dictionary()
+        json_dictionary = Base.to_json_string([dictionary])
+        self.assertDictEqual(
+            dictionary, {'x': 2, 'size': 5, 'id': 1, 'y': 3})
+        self.assertIsInstance(json_dictionary, str)
+        self.assertIsInstance(dictionary, dict)
+        self.assertListEqual(
+            loads(json_dictionary),
+            loads('[{"x": 2, "size": 5, "id": 1, "y": 3}]')
+        )
+
+        json_dictionary = Base.to_json_string([])
+        self.assertEqual(json_dictionary, "[]")
+        json_dictionary = Base.to_json_string([{'1': 2, 'e': 6}])
+        self.assertEqual(json_dictionary, '[{"1": 2, "e": 6}]')
