0817d6dac41d69c2b53d7fd1b767b0eab43f180a
eyadfattah23 <eyadabdelfattah4@gmail.com>
2023-09-08 19:59:29 +0300

    add with grant option

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -6,4 +6,5 @@ CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
 
 GRANT SELECT 
 	ON hbtn_0d_2
-	TO 'user_0d_2'@'localhost';
+	TO 'user_0d_2'@'localhost'
+	WITH GRANT OPTION;
