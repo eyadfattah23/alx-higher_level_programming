19f6920679385262286176f34bdb3d0cd91bf500
eyadfattah23 <eyadabdelfattah4@gmail.com>
2023-09-08 21:01:29 +0300

    revert

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -4,6 +4,6 @@ IF NOT EXISTS 'user_0d_1'@'localhost'
 IDENTIFIED BY 'user_0d_1_pwd';
 
 GRANT ALL PRIVILEGES
-ON *.* TO 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+ON *.* TO 'user_0d_1'@'localhost' WITH GRANT OPTION;
 
 FLUSH PRIVILEGES;
