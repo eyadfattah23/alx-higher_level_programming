97285b5a6bdbb8b839185fc8dff209f11b7ca39e
eyadfattah23 <eyadabdelfattah4@gmail.com>
2023-09-13 00:14:12 +0300

    semistand. fix

--- 0x12-javascript-warm_up/101-call_me_moby.js
+++ 0x12-javascript-warm_up/101-call_me_moby.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 exports.callMeMoby = function (x, theFunction) {
-	let i = 0;
-	for (let i = 0; i < x; i++) {
-		theFunction();
-	}
-}
+  const i = 0;
+  for (let i = 0; i < x; i++) {
+    theFunction();
+  }
+};
