77e894c3efa7fc7fad6d6288afd2aae7766cf2f8
eyadfattah23 <eyadabdelfattah4@gmail.com>
2023-09-12 23:39:31 +0300

    use parse int

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,9 +1,12 @@
 #!/usr/bin/node
 if (process.argv.length < 4) {
-  console.log(0);
+	console.log(0);
 }
 const array = process.argv;
+for (let index = 0; index < array.length; index++) {
+	parseInt(array[index]);
 
+}
 array.sort();
 
 console.log(array[array.length - 2]);
