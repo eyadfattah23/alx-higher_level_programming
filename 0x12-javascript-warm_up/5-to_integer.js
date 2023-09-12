023af6b98e787b8c24456f57955236ec46e90f24
eyadfattah23 <eyadabdelfattah4@gmail.com>
2023-09-12 19:06:07 +0300

    semistandard fix

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,12 +1,11 @@
 #!/usr/bin/node
 let i = 0;
 process.argv.forEach((val, index) => {
-	i += 1;
+  i += 1;
 });
-let number = process.argv[2];
+const number = process.argv[2];
 if (i < 3 || !(parseInt(number))) {
-	console.log('Not a number');
-}
-else {
-	console.log('My number: ' + parseInt(number));
+  console.log('Not a number');
+} else {
+  console.log('My number: ' + parseInt(number));
 }
