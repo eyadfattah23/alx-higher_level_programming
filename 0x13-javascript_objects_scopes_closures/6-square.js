ab8b50f1af4e0dfb7796407443bd065c804528a3
eyadfattah23 <eyadabdelfattah4@gmail.com>
2023-09-14 00:09:50 +0300

    create charPrint(c) that prints the rectangle using the character c
        If c is undefined, use the character X

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,19 @@
+#!/usr/bin/node
+
+const NSquare = require('./5-square');
+class Square extends NSquare {
+  constructor (size) {
+    super(size);
+  }
+
+  charPrint (c) {
+    if (c != undefined) {
+      for (let i = 0; i < this.width; i++) {
+        console.log(c.repeat(this.width));
+      }
+    } else {
+      super.print();
+    }
+  }
+}
+module.exports = Square;
