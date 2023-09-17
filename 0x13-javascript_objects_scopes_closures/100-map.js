74d47d1bc4e2652b8cde2b2f0855eddba97918b8
eyadfattah23 <eyadabdelfattah4@gmail.com>
2023-09-17 14:41:38 +0300

    clean commit tree just pushed them to clean my tree will return to them later

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,6 +1,9 @@
 #!/usr/bin/node
 const array = require('./100-data').list;
+var i = 0;
+function arr_mul(params) {
 
+}
 const nlist = array.map((element) => element * array.indexOf(element));
 
 console.log(array);
