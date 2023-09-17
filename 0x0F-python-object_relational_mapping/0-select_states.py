3166e60eed6a3e3e6ce919fd58b731cca77ce790
eyadfattah23 <eyadabdelfattah4@gmail.com>
2023-09-17 14:42:11 +0300

    create readme db a script that lists all states from the database hbtn_0e_0_usa

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+'''a script that lists all states from the database hbtn_0e_0_usa'''
+import MySQLdb
+import sys
+mysql_username = sys.argv[1]
+mysql_password = sys.argv[2]
+db_name = sys.argv[3]
+db = MySQLdb.connect(host="localhost", port=3306, user=mysql_username,
+                     passwd=mysql_password, db=db_name)
+
+cursor = db.cursor()
+cursor.execute('SELECT * FROM states order by id')
+
+records = cursor.fetchall()
+for record in records:
+    print(record)
