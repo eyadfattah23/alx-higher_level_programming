8ba0c8ee215f15a20d3e861333f3ba2b8e19ba9f
eyadfattah23 <eyadabdelfattah4@gmail.com>
2023-09-18 23:58:25 +0300

    semistand. fix

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
 const array = require('./100-data').list;
 let i = 0;
-function arr_mul(element) {
-	return element * i++;
+function arr_mul (element) {
+  return element * i++;
 }
 const nlist = array.map((element) => element * i++);
 
