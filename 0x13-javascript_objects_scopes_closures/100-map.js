f77102a3001c4017f6a81ced639f047f7d58c4cb
eyadfattah23 <eyadabdelfattah4@gmail.com>
2023-09-18 23:59:16 +0300

    same as last

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,9 +1,6 @@
 #!/usr/bin/node
 const array = require('./100-data').list;
 let i = 0;
-function arr_mul (element) {
-  return element * i++;
-}
 const nlist = array.map((element) => element * i++);
 
 console.log(array);
