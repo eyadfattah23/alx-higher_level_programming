8148c537e554750e7f02d1f4bb3c645931458494
eyadfattah23 <eyadabdelfattah4@gmail.com>
2023-10-18 04:23:53 +0300

    top 3 avgs in july and august

---
+++ 0x0D-SQL_introduction/102-top_city.sql
@@ -0,0 +1,7 @@
+-- script that displays the average temperature (Fahrenheit) by city ordered by temperature 
+
+SELECT TOP 3 city, AVG(value) as avg_temp
+FROM temperatures
+WHERE month = 7 or month = 8
+GROUP BY city
+ORDER BY avg_temp DESC;
