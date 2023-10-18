299a6431e20fe6df8bf5541825ce41694bdef47b
eyadfattah23 <eyadabdelfattah4@gmail.com>
2023-10-18 04:14:48 +0300

    use another method

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -1,6 +1,4 @@
 -- script that converts hbtn_0c_0 database to UTF8 
 
 ALTER TABLE first_table
-  DEFAULT CHARACTER SET utf8mb4,
-  MODIFY name VARCHAR(256)
-    CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
