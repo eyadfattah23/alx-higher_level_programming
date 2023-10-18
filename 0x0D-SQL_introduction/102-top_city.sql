5f33c61a889360c24342f0a857f79daa7137659e
eyadfattah23 <eyadabdelfattah4@gmail.com>
2023-10-18 04:24:53 +0300

    use LIMIT

--- 0x0D-SQL_introduction/102-top_city.sql
+++ 0x0D-SQL_introduction/102-top_city.sql
@@ -1,7 +1,8 @@
 -- script that displays the average temperature (Fahrenheit) by city ordered by temperature 
 
-SELECT TOP 3 city, AVG(value) as avg_temp
+SELECT city, AVG(value) as avg_temp
 FROM temperatures
 WHERE month = 7 or month = 8
 GROUP BY city
-ORDER BY avg_temp DESC;
+ORDER BY avg_temp DESC
+LIMIT 3;
