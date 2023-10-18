37f8b357ffb562dfbac059ef19c00f65ca149a00
eyadfattah23 <eyadabdelfattah4@gmail.com>
2023-10-18 04:15:40 +0300

    add forgotten ;

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -1,4 +1,6 @@
 -- script that converts hbtn_0c_0 database to UTF8 
 
 ALTER TABLE first_table
-CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+  DEFAULT CHARACTER SET utf8mb4,
+  MODIFY name VARCHAR(256)
+    CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL;
