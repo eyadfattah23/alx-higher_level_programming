73ebeed5d3db1e7ba4dc1bdf5e7896fe3f74b97f
eyadfattah23 <eyadabdelfattah4@gmail.com>
2023-10-19 04:06:05 +0300

    remove the table name

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,8 +1,8 @@
 -- script that lists all the cities of California that can be found in the database
 
-SELECT id ,name FROM cities
+SELECT id, name FROM cities
 WHERE state_id = (
 	SELECT id from states
 	WHERE name = 'California'
 )
-ORDER BY cities.id;
+ORDER BY id;
