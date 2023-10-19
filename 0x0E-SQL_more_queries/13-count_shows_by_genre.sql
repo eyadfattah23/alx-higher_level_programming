03960ecac678679d155a8b033ecc814b40ea57e8
eyadfattah23 <eyadabdelfattah4@gmail.com>
2023-10-19 06:26:28 +0300

    list all genres and number of shows linked to each

---
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -0,0 +1,8 @@
+-- script that lists all genres and displays the number of shows linked to each.
+
+SELECT tv_genres.name AS genre, COUNT(show_id) AS number_of_shows
+FROM tv_genres
+LEFT JOIN tv_show_genres
+ON tv_show_genres.genre_id = tv_genres.id
+GROUP BY tv_genres.name
+ORDER BY number_of_shows DESC;
