1562aa4d7355f2de2b36cd7452555b7e5664255e
eyadfattah23 <eyadabdelfattah4@gmail.com>
2023-10-19 07:20:18 +0300

    add another order criteria

--- 0x0E-SQL_more_queries/16-shows_by_genre.sql
+++ 0x0E-SQL_more_queries/16-shows_by_genre.sql
@@ -6,4 +6,4 @@ LEFT OUTER JOIN tv_show_genres
 ON tv_show_genres.show_id = tv_shows.id
 LEFT OUTER JOIN tv_genres
 ON tv_show_genres.genre_id = tv_genres.id
-ORDER BY tv_shows.title;
+ORDER BY tv_shows.title, tv_genres.name;
