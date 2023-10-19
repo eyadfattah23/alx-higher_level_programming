db5c240ea0b3da2ffa720f24593af7715f63fee7
eyadfattah23 <eyadabdelfattah4@gmail.com>
2023-10-19 04:15:45 +0300

    fix typo and add the another table

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,6 +1,6 @@
 -- script that lists all cities contained in the database 
 
 SELECT cities.id, cities.name, states.name
-FROM cities
+FROM cities, states
 WHERE cities.state_id = states.id
-ORDER BY citites.id;
+ORDER BY cities.id;
