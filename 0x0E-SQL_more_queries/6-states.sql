cdb13ef9a865a5f3762f7f1a6bd9285b74429958
eyadfattah23 <eyadabdelfattah4@gmail.com>
2023-10-19 03:42:36 +0300

    fix order

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -1,10 +1,10 @@
 -- script that creates the database hbtn_0d_usa and the table states
 
-CREATE DATABASE hbtn_0d_usa IF NOT EXISTS;
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 
 USE hbtn_0d_usa;
 
-CREATE TABLE states IF NOT EXISTS (
-	id INT NOT NULL AUTO_INCREMENT UNIQUE PRIMARY KEY,
+CREATE TABLE IF NOT EXISTS states(
+	id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
 	name VARCHAR(256) NOT NULL
 );
