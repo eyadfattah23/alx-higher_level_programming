fefc312c3e06ff6e4dd4728d43c36b3e6bc8c93b
eyadfattah23 <eyadabdelfattah4@gmail.com>
2023-10-23 08:52:38 +0300

    DBs

---
+++ 0x0E-SQL_more_queries/hbtn_0d_tvshows.sql
@@ -0,0 +1,41 @@
+-- Create the database if it doesn't exist
+CREATE DATABASE IF NOT EXISTS hbtn_0d_tvshows;
+
+-- Use the database
+USE hbtn_0d_tvshows;
+
+-- Table structure for table `tv_genres`
+DROP TABLE IF EXISTS `tv_genres`;
+CREATE TABLE `tv_genres` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(256) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=latin1;
+
+-- Dumping data for table `tv_genres`
+INSERT INTO `tv_genres` VALUES (1,'Drama'),(2,'Mystery'),(3,'Adventure'),(4,'Fantasy'),(5,'Comedy'),(6,'Crime'),(7,'Suspense'),(8,'Thriller');
+
+-- Table structure for table `tv_shows`
+DROP TABLE IF EXISTS `tv_shows`;
+CREATE TABLE `tv_shows` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `title` varchar(256) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=latin1;
+
+-- Dumping data for table `tv_shows`
+INSERT INTO `tv_shows` VALUES (1,'House'),(2,'Game of Thrones'),(3,'The Big Bang Theory'),(4,'New Girl'),(5,'Silicon Valley'),(6,'Breaking Bad'),(7,'Better Call Saul'),(8,'Dexter'),(9,'Homeland'),(10,'The Last Man on Earth');
+
+-- Table structure for table `tv_show_genres`
+DROP TABLE IF EXISTS `tv_show_genres`;
+CREATE TABLE `tv_show_genres` (
+  `show_id` int(11) NOT NULL,
+  `genre_id` int(11) NOT NULL,
+  KEY `show_id` (`show_id`),
+  KEY `genre_id` (`genre_id`),
+  CONSTRAINT `tv_show_genres_ibfk_1` FOREIGN KEY (`show_id`) REFERENCES `tv_shows` (`id`),
+  CONSTRAINT `tv_show_genres_ibfk_2` FOREIGN KEY (`genre_id`) REFERENCES `tv_genres` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- Dumping data for table `tv_show_genres`
+INSERT INTO `tv_show_genres` VALUES (1,1),(1,2),(2,3),(2,1),(2,4),(3,5),(4,5),(5,5),(6,6),(6,1),(6,7),(6,8),(8,6),(8,1),(8,2),(8,7),(8,8),(10,5),(10,1);
