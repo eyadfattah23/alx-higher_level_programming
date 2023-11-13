84fde4f19483a0727f3f3776f3c5970498167cae
eyadfattah23 <eyadabdelfattah4@gmail.com>
2023-11-13 19:17:11 +0200

    square printing 2 methods

---
+++ 0x12-javascript-warm_up/8-square.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+
+const num = parseInt(process.argv[2]);
+if (process.argv.length === 2 || !num) {
+  console.log('Missing size');
+} else {
+  if (num > 0) {
+    for (let i = 0; i < num; i++) {
+      for (let j = 0; j < num; j++) {
+        process.stdout.write('X');
+      }
+      console.log();
+    }
+  }
+}
