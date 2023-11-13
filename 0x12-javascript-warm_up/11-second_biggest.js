8f34c2c2d5f0228923c6e86f6e55db328ba5514f
eyadfattah23 <eyadabdelfattah4@gmail.com>
2023-11-13 19:45:58 +0200

    last tasks

---
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+if (process.argv.length < 4) {
+  console.log(0);
+} else {
+  const array = process.argv;
+
+  array.sort(function (a, b) { return a - b; });
+
+  console.log(array[array.length - 2]);
+}
---
+++ 0x12-javascript-warm_up/13-add.js
@@ -0,0 +1,4 @@
+#!/usr/bin/node
+exports.add = function add (n, m) {
+  return n + m;
+};
