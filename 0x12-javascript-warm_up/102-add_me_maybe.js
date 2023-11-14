49fec744d819ef42bc971b852b06299eb663a619
eyadfattah23 <eyadabdelfattah4@gmail.com>
2023-11-14 14:18:53 +0200

    create function that increments and calls a function. 2 methods

---
+++ 0x12-javascript-warm_up/102-add_me_maybe.js
@@ -0,0 +1,17 @@
+#!/usr/bin/node
+let num;
+exports.addMeMaybe = function (number, theFunction) {
+  num = function (number) {
+    return number + 1;
+  };
+  theFunction(num(number));
+};
+
+/** #!/usr/bin/node
+let num;
+exports.addMeMaybe = function (number, theFunction) {
+  num = function (number) {
+return number + 1;
+  };
+  theFunction(num(number));
+}; */
