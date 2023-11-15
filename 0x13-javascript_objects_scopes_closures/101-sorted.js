42c95cb19ecaf0b2a1ce166ecf6581687d2073b3
eyadfattah23 <eyadabdelfattah4@gmail.com>
2023-11-15 15:28:04 +0200

    add explanation

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -12,3 +12,6 @@ for (const key in dict) {
   }
 }
 console.log(result);
+
+/** create
+script that imports a dictionary of occurrences by user id and computes a dictionary of user ids by occurrence. */
