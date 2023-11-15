347dea8cf72933436c7f4f22c26a5142b6969008
eyadfattah23 <eyadabdelfattah4@gmail.com>
2023-11-15 14:44:53 +0200

    add 2nd method

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -7,3 +7,12 @@ const result = list.map((x, index) => {
 
 console.log(list);
 console.log(result);
+
+/**
+ * const array = require('./100-data').list;
+let i = 0;
+const nlist = array.map((element) => element * i++);
+
+console.log(array);
+console.log(nlist);
+ */
