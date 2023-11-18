281eada3c5df9c1706ada01f23d07d157d7b0872
eyadfattah23 <eyadabdelfattah4@gmail.com>
2023-11-18 13:06:24 +0200

    Filter states with N

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+'''a script that lists all states from the database hbtn_0e_0_usa'''
+if __name__ == '__main__':
+    import MySQLdb
+    from sys import argv
+    mysql_username = argv[1]
+    mysql_password = argv[2]
+    database_name = argv[3]
+    conn = MySQLdb.connect(host="localhost",
+                           port=3306,
+                           user=mysql_username,
+                           passwd=mysql_password,
+                           db=database_name)
+
+    cur = conn.cursor()
+
+    states = cur.execute(
+        "SELECT * FROM states where name like 'N%' order by id")
+
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
