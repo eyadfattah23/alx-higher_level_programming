570332de2d2a368283ff8eae0571823a293ba9cb
eyadfattah23 <eyadabdelfattah4@gmail.com>
2023-11-18 13:18:53 +0200

    comment

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-'''a script that lists all states from the database hbtn_0e_0_usa'''
+'''a script that lists all states from the database hbtn_0e_0_usa that starts with N'''
 if __name__ == '__main__':
     import MySQLdb
     from sys import argv
