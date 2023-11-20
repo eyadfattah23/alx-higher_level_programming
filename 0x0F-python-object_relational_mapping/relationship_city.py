5edcbd7d41efd50799add51031a3731f463a252b
eyadfattah23 <eyadabdelfattah4@gmail.com>
2023-11-20 05:53:58 +0200

    City relationship

---
+++ 0x0F-python-object_relational_mapping/relationship_city.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+'''class definition of a City'''
+
+from relationship_state import Base
+from sqlalchemy import create_engine,\
+    MetaData, Table, Column, Integer, String
+from sqlalchemy import ForeignKey
+from sqlalchemy.orm import relationship
+
+
+class City(Base):
+    '''
+        inherits from Base (imported from model_state)
+
+        links to the MySQL table cities
+
+        class attribute id that represents a column of
+        an auto-generated,
+        unique integer,
+        can’t be null and
+        is a primary key
+
+        class attribute name that represents a column of
+        a string of 128 characters
+        and can’t be null
+
+        class attribute state_id that represents
+        a column of an integer,
+        can’t be null
+        and is a foreign key to states.id
+    '''
+    __tablename__ = 'cities'
+    id = Column(Integer, primary_key=True,
+                nullable=False, autoincrement=True)
+    name = Column(String(128), nullable=False)
+    state_id = Column(Integer, ForeignKey("states.id"), nullable=False)
