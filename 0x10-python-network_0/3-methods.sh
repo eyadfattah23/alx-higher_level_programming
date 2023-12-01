9339381f19e40a8ff62b6436326e7c376efe9293
eyadfattah23 <eyadabdelfattah4@gmail.com>
2023-12-01 17:47:22 +0200

    combine s and i options

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # script that takes in a URL and displays all HTTP methods the server will accept.
-curl -s -i -L -X OPTIONS $1 | grep "allow" | sed 's/Allow: //i'
+curl -si -L -X OPTIONS $1 | grep -i "allow" | sed 's/Allow: //i'
