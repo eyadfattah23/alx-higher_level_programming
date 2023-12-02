300214c3f0cab42d82ede427458dd71d41d0060f
eyadfattah23 <eyadabdelfattah4@gmail.com>
2023-12-02 13:24:57 +0200

    TRY =

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # script that takes in a URL, sends a POST request to the passed URL
-curl -d "email: test@gmail.com" -d "subject: I will always be here for PLD" -s "$1"
+curl -d "email=test@gmail.com" -d "subject=I will always be here for PLD" -s "$1"
