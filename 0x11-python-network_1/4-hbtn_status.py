73cb343d783dcb78977784223f81440bd5f11576
eyadfattah23 <eyadabdelfattah4@gmail.com>
2023-12-06 21:02:10 +0200

    fix typo

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -7,5 +7,5 @@ if __name__ == "__main__":
 
     req = requests.get('https://alx-intranet.hbtn.io/status')
     print("Body response:")
-    print(f"\t- type: {req.text}")
+    print(f"\t- type: {type(req.text)}")
     print(f"\t- content: {req.text}")
