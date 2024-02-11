7e098a859493d88389eb58664ad10fa293467a12
eyadfattah23 <eyadabdelfattah4@gmail.com>
2024-02-11 11:00:22 +0200

    updates the text of the <header>  *  to New Header!!! when the user clicks on DIV#update_header

---
+++ 0x15-javascript-web_jquery/6-script.js
@@ -0,0 +1,8 @@
+/**script that updates the text of the <header>
+ *  to New Header!!! when the user clicks on DIV#update_header */
+
+$(function () {
+	$('#update_header').on('click', function () {
+		$('header').text('New Header!!!')
+	})
+})
