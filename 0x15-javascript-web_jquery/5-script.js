dc73809b1ee65eb0eb1e6e4660c5b997e36845dc
eyadfattah23 <eyadabdelfattah4@gmail.com>
2024-02-11 10:37:01 +0200

    script that adds a <li> element to a list when the user clicks on the tag

---
+++ 0x15-javascript-web_jquery/5-script.js
@@ -0,0 +1,10 @@
+/**
+ * script that adds a <li> element to a list
+ * when the user clicks on the tag DIV#add_item
+ */
+
+$(function () {
+	$('#add_item').on('click', function () {
+		$('ul.my_list').append('<li>Item</li>')
+	})
+})
