fc72ba33ebd0088a46a267a27ae795878f2e5524
eyadfattah23 <eyadabdelfattah4@gmail.com>
2024-02-11 10:32:11 +0200

    script that toggles the class of the <header> between red and green

---
+++ 0x15-javascript-web_jquery/4-script.js
@@ -0,0 +1,18 @@
+/**
+ * script that toggles the class of the <header>
+ *  when the user clicks on the tag DIV#toggle_header
+ */
+$(function () {
+	$("#toggle_header").on("click", function () {
+
+		if ($('header').hasClass('red')) {
+
+			$('header').removeClass('red');
+			$('header').addClass('green');
+		} else {
+			$('header').removeClass('green');
+			$('header').addClass('red');
+
+		}
+	});
+})
