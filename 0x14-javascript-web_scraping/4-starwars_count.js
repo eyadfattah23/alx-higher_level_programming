bbc4daffa284b968f3c4a185ab618dbf14c8fabe
eyadfattah23 <eyadabdelfattah4@gmail.com>
2024-06-14 09:48:40 +0300

    use String Matching Approach:

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -8,12 +8,12 @@
  *  https://swapi-api.alx-tools.com/api/films/
  */
 const request = require('request');
-const url = 'https://swapi-api.alx-tools.com/api/people/18';
+const url = process.argv[2];
 
 request(url, function (error, response, body) {
   if (error) {
     console.log(error);
   } else {
-    console.log(JSON.parse(body).films.length);
+    console.log(body.split('/people/18/').length - 1);
   }
 });
