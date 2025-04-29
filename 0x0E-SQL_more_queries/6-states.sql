7353d6860a04602970e65f7c2c423d6924e64093
eyadfattah23 <eyadabdelfattah4@gmail.com>
2025-04-29 17:35:39 +0300

    add another way for the same task

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -8,3 +8,9 @@ CREATE TABLE IF NOT EXISTS states(
 	id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
 	name VARCHAR(256) NOT NULL
 );
+
+-- create table states (
+-- id INT NOT NULL AUTO_INCREMENT,
+-- name VARCHAR(256) NOT NULL,
+-- PRIMARY KEY (id)
+-- );
