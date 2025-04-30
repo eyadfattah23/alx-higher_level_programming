d3c093158215ca71458bc78e0068b3bbdf53d1d4
eyadfattah23 <eyadabdelfattah4@gmail.com>
2025-04-30 15:25:50 +0300

    add another solution

--- 0x0E-SQL_more_queries/102-rating_shows.sql
+++ 0x0E-SQL_more_queries/102-rating_shows.sql
@@ -7,3 +7,6 @@ LEFT JOIN tv_show_ratings
 ON tv_show_ratings.show_id = tv_shows.id
 GROUP BY tv_shows.title
 ORDER BY rating DESC;
+
+
+--SELECT tv_shows.title AS title, SUM(tv_show_ratings.rate) AS rate  FROM tv_show_ratings  JOIN tv_shows ON tv_shows.id = tv_show_ratings.show_id GROUP BY title ORDER BY rate DESC;
